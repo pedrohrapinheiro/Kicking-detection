ae91ae8c848af765e4b95ebd3f92bf39c05369a3
Foxpinheiro <pedrohrapinheiro@gmail.com>
2026-06-28 15:56:00 -0300

    Landmarks and textPutting

--- main.py
+++ main.py
@@ -8,17 +8,38 @@ import cv2 as cv
 
 def drawing_landmarks(imageRGB, detected_result):
     pose_landmarks_list = detected_result.pose_landmarks
+
     annotated_image = np.copy(imageRGB)
 
     pose_landmark_style = drawing_styles.get_default_pose_landmarks_style()
-    pose_connections_style = drawing_utils.DrawingSpec(color=(80, 22, 10), thickness=2)
+    pose_connections_style = drawing_utils.DrawingSpec(color=(0, 255, 0), thickness=2)
 
-    for landmarks in pose_landmarks_list:
+    if detected_result.pose_landmarks:
+
+        first_person_landmarks = detected_result.pose_landmarks[0]
+
+        left_knee_index = 25
+        right_knee_index = 26
+        left_foot_index = 31
+        right_foot_index = 32
+        left_knee = first_person_landmarks[left_knee_index]
+        right_knee = first_person_landmarks[right_knee_index]
+
+        left_foot = first_person_landmarks[left_foot_index]
+        right_foot = first_person_landmarks[right_foot_index]
+
+        if left_knee.y < left_foot.y or right_knee.y < right_foot.y:
+            cv.putText(annotated_image, 'Knee Up', (0, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv.LINE_AA)
+        if left_knee.visibility < 0.5 or right_knee.visibility < 0.5:
+            cv.putText(annotated_image, 'Knee Down', (0, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv.LINE_AA)
+
+
+    for landmarks in pose_landmarks_list: 
         drawing_utils.draw_landmarks(
-            annotated_image,
-            landmarks,
-            vision.PoseLandmarksConnections.POSE_LANDMARKS,
-            pose_landmark_style,
+            annotated_image, 
+            landmarks, 
+            vision.PoseLandmarksConnections.POSE_LANDMARKS, 
+            pose_landmark_style, 
             pose_connections_style)
     return annotated_image
 
