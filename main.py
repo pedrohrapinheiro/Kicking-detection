de1f09d9ba59d92e9f48928798906356ad00f43e
Foxpinheiro <pedrohrapinheiro@gmail.com>
2026-06-28 15:10:31 -0300

    added tracking

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import numpy as np
 from mediapipe.tasks.python.vision import drawing_utils
 from mediapipe.tasks.python.vision import drawing_styles
 from mediapipe.tasks.python import vision
+import mediapipe as mp
+from mediapipe.tasks import python
+import cv2 as cv
 
 def drawing_landmarks(imageRGB, detected_result):
     pose_landmarks_list = detected_result.pose_landmarks
@@ -18,3 +21,34 @@ def drawing_landmarks(imageRGB, detected_result):
             pose_landmark_style,
             pose_connections_style)
     return annotated_image
+
+base_options = python.BaseOptions(model_asset_path='pose_landmarker_heavy.task')
+options = vision.PoseLandmarkerOptions(base_options=base_options, output_segmentation_masks=True)
+detector = vision.PoseLandmarker.create_from_options(options)
+
+cap = cv.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+    mp_image = mp.Image(
+        image_format=mp.ImageFormat.SRGB,
+        data=frame_rgb
+    )
+
+    detected_result = detector.detect(mp_image)
+
+    annotated_image = drawing_landmarks(mp_image.numpy_view(), detected_result)
+
+    cv.imshow('Output', cv.cvtColor(annotated_image, cv.COLOR_RGB2BGR))
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
